91aba324a723ef3173f913e4df4a23f0a5f372f8
Emma Paris <anupam@matrixnmedia.com>
2019-01-31 19:55:09 +0530

    FlutterWallpaperApplicationStarts

--- lib/aftersplash/aftersplash.dart
+++ lib/aftersplash/aftersplash.dart
@@ -5,22 +5,38 @@ class AfterSplash extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    return Scaffold(
-      body: Container(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Welcome to Flutter',
+      home: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("images/aftrsplshbckgrnd.png"),
-            fit: BoxFit.cover,
+            image: DecorationImage(
+                image: AssetImage("images/aftrsplshbckgrnd.png"), fit: BoxFit.cover)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            title: Text('Future of Wallpaper Application',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                decoration: TextDecoration.none,
+                fontWeight: FontWeight.w800,
+                fontFamily: 'Aleo'
+            ),
+            ),
+            //centerTitle: true,
+            leading: IconButton(
+                icon: Icon(
+                  Icons.list,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+
+                }
+                ),
           ),
-        ),
-        child: Column(
-          children: <Widget>[
-
-
-
-
-
-          ],
         ),
       ),
     );
